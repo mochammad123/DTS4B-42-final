48843ed4584db3206505b8a3d24aa90a49c90a9b
patriilham <patriilham002@gmail.com>
2022-10-22 21:46:10 +0700

    Login and Register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/landing_components/home/Home";
 import Detail from "./components/landing_components/details/Detail";
+import Login from "./components/landing_components/login/Login";
+import Register from "./components/landing_components/register/register";
+
 
 
 function App() {
@@ -11,6 +14,10 @@ function App() {
         <Routes>
           <Route exact path="/home" element={<Home />} />
           <Route exact path="/detail" element={<Detail />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+
+
         </Routes>
       </Router>
     </div>
