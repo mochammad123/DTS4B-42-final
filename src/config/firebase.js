70e224887a3c47c61dddadd81191fef4bc37b010
Moch. Faisal Ediansyah <faisalmochammad54@gmail.com>
2022-10-25 08:40:18 +0700

    firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCNEz4RN_4g_U1kGeXl_8FFVtpiEWfF9KY",
-  authDomain: "newsdts4b-reactfinal.firebaseapp.com",
-  projectId: "newsdts4b-reactfinal",
-  storageBucket: "newsdts4b-reactfinal.appspot.com",
-  messagingSenderId: "675254212243",
-  appId: "1:675254212243:web:7133acc6bfb93a46741f66",
+  apiKey: "AIzaSyDPaNcu_gX6Zny-iKbv8soX7ubGZMnkPxw",
+  authDomain: "dts4b-42-final.firebaseapp.com",
+  projectId: "dts4b-42-final",
+  storageBucket: "dts4b-42-final.appspot.com",
+  messagingSenderId: "741171675889",
+  appId: "1:741171675889:web:790b592cb61246fd539f50",
+  measurementId: "G-KGFNTNS32J",
 };
 
 // Initialize Firebase
